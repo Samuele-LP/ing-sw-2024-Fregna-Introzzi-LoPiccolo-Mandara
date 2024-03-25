6c731f5d671fc44a98beffbd479afc60ed97816e
Samuele Lo Piccolo <samuele.lopiccolo@mail.polimi.it>
2024-03-25 22:21:30 +0100

    Added class Point

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Point.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Point.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.model;
 
 public class Point {
+    private final int xCoord;
+    private final int yCoord;
+    public Point(int x, int y){
+        xCoord=x;
+        yCoord=y;
+    }
+    public int getX(){return xCoord;}
+    public int getY(){return yCoord;}
+    @Override
+    public String toString(){
+        return "X:"+xCoord+"  Y:"+yCoord;
+    }
 }
