e9ed2be9493c3e95642fe6a2719ce0c64f17e6e3
federicointrozzi <federico.introzzi@mail.polimi.it>
2024-04-02 14:18:35 +0200

    Game state enum and changes to temp Game class added

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,38 +1,87 @@
 package it.polimi.ingsw.model;
 
 import java.util.*;
+import java.util.ArrayList;
+import it.polimi.ingsw.model.enums.GameState;
 public class Game {
     /**
      *  Data
      */
+    private GameState currentState=GameState.SETUP;
     public int numPlayers;
-    public int turn;
+    public int currentPlayerIndex;
+    private Deck<ObjectiveCard> objectiveDeck;
+    private Deck<GoldCard> goldDeck;
+    private Deck<ResourceCard> resourceDeck;
+    private List<StartingCard> startingCards;
     public List<Player> players;
+    private ScoreTrack scoreTrack;
 
-    public Game(int num){ this.numPlayers = num; };
+    public Game(int num){
+        this.numPlayers = num;
+        this.players = new ArrayList<>();
+        this.currentPlayerIndex = 0;
+    };
 
+    public void startGame(Deck<PlayableCard> playableDeck, Deck<ObjectiveCard> objectiveDeck) {
+        this.playableDeck = playableDeck;
+        this.objectiveDeck = objectiveDeck;
+        this.startingCards = new ArrayList<>();
+        currentState = GameState.SETUP;
+        setupPlayers();
+    }
     /**
      *  Set-up
      */
-    public void start_game(/* Deck: GoldenDeck1, Deck: ResourceDeck1, Deck: ObjectiveDeck1*/){
-        /* ----------------------------------------------------------( 1 )-----------------------------------*/
-        switch(numPlayers) {
-            /*------------------------- to fix the setting of the keys --------------------*/
+    private void setupPlayers() {
+        switch (numPlayers) {
             case 2:
-                /* TODO */
+                scoreTrack = new ScoreTrack("Player 1", "Player 2");
+                break;
+            case 3:
+                scoreTrack = new ScoreTrack("Player 1", "Player 2", "Player 3");
+                break;
+            case 4:
+                scoreTrack = new ScoreTrack("Player 1", "Player 2", "Player 3", "Player 4");
+                break;
+            default:
+                System.out.println("Invalid number of players!");
+                return;
+        }
+
+        /*manages the init phase of the game, players are added, startingCards are dealt
+            and the player has to decide between the secretObjective dealt*/
+
+        for (int i = 0; i < numPlayers; i++) {
+            Player player = new Player("Player " + (i + 1));
+            players.add(player);
+            dealStartingCards(player);
+            dealSecretObjective(player);
+        }
+        currentState = GameState.GAME_ROUND;
+    }
+ /*
+                        VITALIANO
+
+ public void start_game(*//* Deck: GoldenDeck1, Deck: ResourceDeck1, Deck: ObjectiveDeck1*//*){
+        *//* ----------------------------------------------------------( 1 )-----------------------------------*//*
+        switch(numPlayers) {
+            *//*------------------------- to fix the setting of the keys --------------------*//*
+            case 2:
+                *//* TODO *//*
                 ScoreTrack Table2 = new ScoreTrack("1","2");
                 break;
             case 3:
-                /* TODO */
+                *//* TODO *//*
                 ScoreTrack Table3 = new ScoreTrack("1","2","3");
                 break;
             case 4:
-                /* TODO */
+                *//* TODO *//*
                 ScoreTrack Table4 = new ScoreTrack("1","2","3","4");
                 break;
             default:
                 System.out.println("Il numero non è valido");
-        }
+        }*/
         /**
          *   phase of game
          */
@@ -95,22 +144,83 @@ public class Game {
          *  --- put the pawn on the table ---
          */
 
-    }
-    public void start_game_round(){
-        /* the round should be a Class with the 2 methods for placing_cards and drawing */
+        /**
+         * This class aim to manages the game rounds and the overall flow of the game
+         *
+         */
+
+
+        public void gameRound() {
+            if (currentState != GameState.GAME_ROUND) {
+                return;
+            }
+
+            Player currentPlayer = players.get(currentPlayerIndex);
+
+            // PlayerTurn TODO
+
+
+            if (isRoundOver()) {
+                //increase the currentPlayer index, %numPlayers guarantees that
+                // the index never overtake the numbers of players
+                currentPlayerIndex = (currentPlayerIndex + 1) % numPlayers;
+                if (isGameOver()) {
+                    currentState = GameState.FINAL_PHASE;
+                    calculateFinalPoints();
+                    declareWinner();
+                    currentState = GameState.FINISHED;
+                } else {
+                    currentState = GameState.GAME_ROUND;
+                }
+            }
+        }
+
+    /**
+     * getter implemented to always know the state of the game
+     * @return currentState
+     */
+    public GameState getGameState() {
+        return currentState;
     }
 
-    public void placing_phase(int player){
-        /* TODO */
+    /**
+     * this method checks if the player round is ended or not
+     * @return true if the round is ended, false otherwise
+     */
+    private boolean isRoundOver() {
+        /*TODO: bisogna controllare che il giocatore abbia:
+         *  giocato e pescato una carta. ANche solo la pescata(visto che viene dopo assiomaticamente)
+         * ma potrebbero esserci dell'eccezioni da gestire (giocatore pesca senza giocare)
+         * */
+
+        return false;
     }
 
-    public void draw_phase(int ID_player, int DrawChoice){
-        /* TODO */
+    /**
+     * this method checks if the game is still on or the final phase is reached
+     * @return true if the final Phase is reached, false otherwise
+     */
+
+    private boolean isGameOver() {
+        for (Player player : players) {
+            if (player.getPoints() >= 20) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void begin_final_phase(){
-        /* TODO */
+    /**
+     * This method aims to calculate the objective points in the final phase
+     */
+    private void calculateFinalPoints() {
+            if(currentState != GameState.FINAL_PHASE)
+                return;
+        //TODO
+        // After reaching 20 points, the finalphase starts and the
+        //points given by the objective are calculated ib this method
     }
+
 
     /**
      * Declare Winner based on the dinamic list of players (that can have 2 or 3 or 4 elements).
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/GameState.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.model.enums;
+
+public enum GameSTate {
+    SETUP, GAME_ROUND, FINAL_PHASE, FINISHED
+}
