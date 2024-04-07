26af126d1652dda0838be38385b2a084516fc989
federicointrozzi <federico.introzzi@mail.polimi.it>
2024-04-07 15:34:13 +0200

    Fixed game and minor issues

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
@@ -4,18 +4,17 @@ import it.polimi.ingsw.exceptions.IllegalStartingCardException;
 import it.polimi.ingsw.model.enums.GameState;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class Game {
-    /**
-     *  Data
-     */
     private GameState currentState=GameState.SETUP;
     public int numPlayers;
     public int currentPlayerIndex;
     private Deck objectiveDeck;
     private Deck goldDeck;
     private Deck resourceDeck;
-    private List<StartingCard> startingCards;
     public List<Player> players;
     private ScoreTrack scoreTrack;
 
@@ -25,34 +24,38 @@ public class Game {
         this.currentPlayerIndex = 0;
     };
 
-    public void startGame(Deck goldDeck, Deck objectiveDeck, Deck resourceDeck) throws Exception, IllegalStartingCardException {
+    public void startGame(Deck goldDeck, Deck objectiveDeck, Deck resourceDeck, String username1, String username2, String username3, String username4) throws Exception, IllegalStartingCardException {
         this.resourceDeck = resourceDeck;
         this.goldDeck = goldDeck;
         this.objectiveDeck = objectiveDeck;
-        this.startingCards = new ArrayList<>();
         currentState = GameState.SETUP;
-        setupScoreTrack();
+        setupScoreTrack(username1, username2, username3, username4);
         setupDecks();
-        setupPlayers();
+        setupPlayers(username1, username2, username3, username4);
     }
     /**
      *  Set-up
      */
-    private void setupScoreTrack(){
+    private void setupScoreTrack(String username1, String username2, String username3, String username4){
         switch (numPlayers) {
             case 2:
-                scoreTrack = new ScoreTrack("Player 1", "Player 2");
+                scoreTrack = new ScoreTrack(username1, username2);
                 break;
             case 3:
-                scoreTrack = new ScoreTrack("Player 1", "Player 2", "Player 3");
+                scoreTrack = new ScoreTrack(username1, username2, username3);
                 break;
             case 4:
-                scoreTrack = new ScoreTrack("Player 1", "Player 2", "Player 3", "Player 4");
+                scoreTrack = new ScoreTrack(username1, username2, username3, username4);
                 break;
             default:
                 System.out.println("Invalid number of players!");
         }
     }
+
+    /**
+     *
+     * @throws Exception
+     */
 
     private void setupDecks() throws Exception {
         resourceDeck.shuffle();
@@ -62,152 +65,143 @@ public class Game {
         goldDeck.setFirstVisible();
         goldDeck.setSecondVisible();
         objectiveDeck.shuffle();
+        objectiveDeck.setFirstVisible();
+        objectiveDeck.setSecondVisible();
     }
 
+    /**
+     *
+     * @throws IllegalStartingCardException
+     * @throws Exception
+     */
 
-    private void setupPlayers() throws IllegalStartingCardException {
+    private void setupPlayers(String username1, String username2, String username3, String username4) throws Exception, IllegalStartingCardException {
 
         /*manages the init phase of the game, players are added, startingCards are dealt
-            and the player has to decide between the secretObjective dealt*/
+           and the player has to decide between the secretObjective dealt*/
+
+        String[] currentUsername = {username1, username2, username3, username4};
 
         for (int i = 0; i < numPlayers; i++) {
-            Player player = new Player("Player " + (i + 1));
+            Player player = new Player(currentUsername[i]);
             players.add(player);
-            dealStartingCards(player);
-            drawPlayableCards();
+            drawInitialCards(player);
             dealSecretObjective(player);
         }
         currentState = GameState.GAME_ROUND;
     }
 
-    private void drawPlayableCards() {
+    /**
+     *
+     * @param player
+     * @return
+     * @throws Exception
+     */
+
+    private Card[] drawInitialCards(Player player) throws Exception {
+        Card[] drawnCard = null;
+        int i=0;
+        for(i = 0; i<2; i++){
+            drawnCard[i]=resourceDeck.draw(0);
+        }
+        drawnCard[i+1]=goldDeck.draw(0);
+        return drawnCard;
         //player draws 2 resource cards and 1 gold one
     }
 
+    /**
+     *
+     * @param player
+     * @return
+     * @throws Exception
+     */
 
-    private void dealStartingCards(Player player) {
+    public ObjectiveCard[] dealSecretObjective(Player player) throws Exception {
+        ObjectiveCard[] objectiveOptions = null;
+        for (int i = 0; i < 2; i++) {
+            objectiveOptions[i] = (ObjectiveCard) objectiveDeck.draw(0);
+        }
+        return  objectiveOptions;
     }
 
-    private void dealSecretObjective(Player player) {
-    }
- /*
-                        VITALIANO
+    /**
+     *
+     * @param player
+     * @param secretObjective
+     */
 
- public void start_game(*//* Deck: GoldenDeck1, Deck: ResourceDeck1, Deck: ObjectiveDeck1*//*){
-        *//* ----------------------------------------------------------( 1 )-----------------------------------*//*
-        switch(numPlayers) {
-            *//*------------------------- to fix the setting of the keys --------------------*//*
-            case 2:
-                *//* TODO *//*
-                ScoreTrack Table2 = new ScoreTrack("1","2");
+    private void placeSecretObjective(Player player, ObjectiveCard secretObjective){
+        player.setSecretObjective(secretObjective);
+    }
+
+
+    public void drawCard(Player player, int drawChoice, String typeOfCard) throws Exception {
+        Card drawncard=null;
+        switch(typeOfCard){
+            case "resource":
+                drawncard=resourceDeck.draw(drawChoice);
+                player.receiveDrawnCard((PlayableCard) drawncard);
                 break;
-            case 3:
-                *//* TODO *//*
-                ScoreTrack Table3 = new ScoreTrack("1","2","3");
-                break;
-            case 4:
-                *//* TODO *//*
-                ScoreTrack Table4 = new ScoreTrack("1","2","3","4");
+            case "gold":
+                drawncard=goldDeck.draw(drawChoice);
+                player.receiveDrawnCard((PlayableCard) drawncard);
                 break;
             default:
-                System.out.println("Il numero non è valido");
-        }*/
-        /**
-         *   phase of game
-         */
-        /*
-         *  Random rand = new Random();
-         *  int randomNumber = rand.nextInt(2) + 2;
-         *  int turn;
-         *  turn = randomNumber;
-        /*
-         *  SETUP GAME
-         *
-         * -----------------------------------------------------------( 2 )--------------------------------------
-         *  GoldenDeck1.shuffle();               (?)
-         *  GoldenDeck1.getFirstVisible();
-         *  GoldenDeck1.getSecondVisible();
-         *  ResourceDeck1.shuffle();             (?)
-         *  ResourceDeck1.getFirstVisible();
-         *  ResourceDeck1.getSecondVisible();
-         * -----------------------------------------------------------( 3 )---------------------------------------
-         *
-         * switch(numPlayers) {
-         *   case 2:
-         *       --- DRAW + PLACING
-         *  Random rand = new Random();
-         *  int randomNumber = rand.nextInt(2) + 2;
-         *  int turn;
-         *  turn = randomNumber;
-         *
-         *
-         *   case 3:
-         *       --- DRAW + PLACING
-         *       break;
-         *   case 4:
-         *       --- DRAW + PLACING
-         *       break;
-         *   default:
-         *       System.out.println("Il numero non è valido");
-         *  }
-         *
-         *
-         *
-         *
-         *  ObjectiveCard1.shuffle();
-         *  ObjectiveCard1.getFirstVisible();
-         *  ObjectiveCard1.getSecondVisible();
-         *  if(numPlayers == 2)
-         * {
-         *      --- DRAW 2 CARDS AND CHOICE FOR 2 PLAYERS
-         * }
-         *  if(numPlayers == 3)
-         * {
-         *      --- DRAW 2 CARDS AND CHOICE FOR 3 PLAYERS
-         * }
-         *  if(numPlayers == 4)
-         * {
-         *      --- DRAW 2 CARDS AND CHOICE FOR 4 PLAYERS
-         * }
-         *
-         *
-         *  --- put the pawn on the table ---
-         */
-
-        /**
-         * This class aim to manages the game rounds and the overall flow of the game
-         *
-         */
+                System.out.println("Invalid type of playable card");
+        }
+    }
 
 
-        public void gameRound() {
-            if (currentState != GameState.GAME_ROUND) {
-                return;
-            }
-            int phaseCounter=0;
-            Player currentPlayer = players.get(currentPlayerIndex);
-
-            // PlayerTurn TODO
-            //play card
-            //draw card
-            //i can use a counter to increase in each phase, if =2
-            //round is finished and go to the next round, setting counter back to 0
 
 
-            if (isRoundOver(phaseCounter)) {
-                //increase the currentPlayer index, %numPlayers guarantees that
-                // the index never overtake the numbers of players
-                currentPlayerIndex = (currentPlayerIndex + 1) % numPlayers;
-                if (isGameOver()) {
-                    currentState = GameState.FINAL_PHASE;
-                    calculateFinalPoints();
-                    declare_winner();
-                    currentState = GameState.FINISHED;
-                } else {
-                    currentState = GameState.GAME_ROUND;
-                }
+    public void playCard(String playerName, Card card){
+        return;
+    }
+
+
+    /**
+     *
+     * @return finalPoints map with each player associated with his final points
+     * @throws IllegalStateException
+     */
+    private Map<Player, Integer> calculateFinalPoints() throws IllegalStateException {
+            if(currentState != GameState.FINAL_PHASE)
+                throw new IllegalStateException("Not in the final phase yet");
+            Card common1 = objectiveDeck.getFirstVisible();
+            Card common2 = objectiveDeck.getSecondVisible();
+        Map<Player, Integer> finalPoints = new HashMap<>();
+        for(Player player: players){
+            player.calculateSecretObjective();
+            player.calculateCommonObjectives((ObjectiveCard) common1, (ObjectiveCard) common2);
+            int totalPoints = player.getPoints();
+            finalPoints.put(player, totalPoints);
+        }
+        return finalPoints;
+    }
+
+
+
+    public void gameRound() {
+        if (currentState != GameState.GAME_ROUND) {
+            return;
+        }
+        int phaseCounter=0;
+        Player currentPlayer = players.get(currentPlayerIndex);
+
+        if (isRoundOver(phaseCounter)) {
+            //increase the currentPlayer index, %numPlayers guarantees that
+            // the index never overtake the numbers of players
+            currentPlayerIndex = (currentPlayerIndex + 1) % numPlayers;
+            if (isGameOver()) {
+                currentState = GameState.FINAL_PHASE;
+                calculateFinalPoints();
+                declare_winner();
+                currentState = GameState.FINISHED;
+            } else {
+                currentState = GameState.GAME_ROUND;
             }
         }
+    }
 
     /**
      * getter implemented to always know the state of the game
@@ -243,24 +237,12 @@ public class Game {
     }
 
     /**
-     * This method aims to calculate the objective points in the final phase
-     */
-    private void calculateFinalPoints() {
-            if(currentState != GameState.FINAL_PHASE)
-                return;
-        //TODO
-        // After reaching 20 points, the finalphase starts and the
-        //points given by the objective are calculated ib this method
-    }
-
-
-    /**
      * Declare Winner based on the dinamic list of players (that can have 2 or 3 or 4 elements).
      * The only information that is saved of the winner is the name.
      * In case of "multiple winners", they can all be found in the List of names "idWinners".
      */
     private void declare_winner(){
-        List<String> idWinners = new List<>(String);
+        List<String> idWinners = new ArrayList<>();
         idWinners.addFirst(players.getFirst().getName());
 
         //NB: "i-1" instead of "i+1" is important so the "OutOfIndex" error should never happen in any circumstance
@@ -271,58 +253,12 @@ public class Game {
                 idWinners.add(players.get(i).getName());
             }
         }
-
-        /*
-         *  VITALIANO MANDARA:
-         *
-         * -- 2 --
-         * if(num_Players==2){
-         * max = player1.currentPoints()
-         * winner = player1.id;
-         * if(player2.currentPoints() > max){
-         * max = player2.currentPoints();
-         * winner = player2.id;
-         *   }
-         * }
-         *
-         * -- 3 --
-         *
-         * if(num_Players==3){
-         * max = player1.currentPoints()
-         * winner = player1.id;
-         * if(player2.currentPoints() > max){
-         * max = player2.currentPoints()
-         * winner = player2.id
-         *   }
-         * else if (player3.currentPoints() > max){
-         * max = player3.currentPoints()
-         * winner = player3.id
-         *   }
-         * }
-         *
-         * -- 4 --
-         *
-         * if(num_Players==4){
-         * max = player1.currentPoints()
-         * winner = player1.id;
-         * if(player2.currentPoints() > max){
-         * max = player2.currentPoints()
-         * winner = player2.id
-         *   }
-         * if (player3.currentPoints() > max){
-         * max = player3.currentPoints()
-         * winner = player3.id
-         *   }
-         * if (player4.currentPoints() > max){
-         * max = player3.currentPoints()
-         * winner = player3.id
-         *   }
-         * }
-         *
-         *
-         */
-
     }
+
+    /**
+     *
+     * @return
+     */
 
     public String get_next_winner_name(){
         String next_winner = players.getFirst().getName();
