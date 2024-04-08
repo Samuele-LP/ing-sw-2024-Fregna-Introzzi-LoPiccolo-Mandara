e91832a21249f07c4aaddafc15bc787c24a98e14
federicointrozzi <federico.introzzi@mail.polimi.it>
2024-04-08 23:54:32 +0200

    Fixed calculateFinalPoints method and other minor class issues

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Game.java
@@ -22,12 +22,12 @@ public class Game {
         this.numPlayers = num;
         this.players = new ArrayList<>();
         this.currentPlayerIndex = 0;
-    };
-
-    public void startGame(Deck goldDeck, Deck objectiveDeck, Deck resourceDeck, String username1, String username2, String username3, String username4) throws Exception, IllegalStartingCardException {
         this.resourceDeck = resourceDeck;
         this.goldDeck = goldDeck;
         this.objectiveDeck = objectiveDeck;
+    }
+
+    public void startGame(Deck goldDeck, Deck objectiveDeck, Deck resourceDeck, String username1, String username2, String username3, String username4) throws Exception, IllegalStartingCardException {
         currentState = GameState.SETUP;
         setupScoreTrack(username1, username2, username3, username4);
         setupDecks();
@@ -94,7 +94,7 @@ public class Game {
     /**
      *
      * @param player
-     * @return
+     * @return drawnCard
      * @throws Exception
      */
 
@@ -102,6 +102,7 @@ public class Game {
         Card[] drawnCard = null;
         int i=0;
         for(i = 0; i<2; i++){
+            assert drawnCard != null;
             drawnCard[i]=resourceDeck.draw(0);
         }
         drawnCard[i+1]=goldDeck.draw(0);
@@ -112,13 +113,14 @@ public class Game {
     /**
      *
      * @param player
-     * @return
+     * @return objectiveOptions
      * @throws Exception
      */
 
     public ObjectiveCard[] dealSecretObjective(Player player) throws Exception {
         ObjectiveCard[] objectiveOptions = null;
         for (int i = 0; i < 2; i++) {
+            assert objectiveOptions != null;
             objectiveOptions[i] = (ObjectiveCard) objectiveDeck.draw(0);
         }
         return  objectiveOptions;
@@ -160,23 +162,22 @@ public class Game {
 
 
     /**
-     *
-     * @return finalPoints map with each player associated with his final points
+     *  Calculate the points in the end phase of the game by adding the points given by private and common objectives
+     *  and update the scoretrack with them
      * @throws IllegalStateException
      */
-    private Map<Player, Integer> calculateFinalPoints() throws IllegalStateException {
+    private void calculateFinalPoints() throws IllegalStateException {
             if(currentState != GameState.FINAL_PHASE)
                 throw new IllegalStateException("Not in the final phase yet");
             Card common1 = objectiveDeck.getFirstVisible();
             Card common2 = objectiveDeck.getSecondVisible();
-        Map<Player, Integer> finalPoints = new HashMap<>();
         for(Player player: players){
             player.calculateSecretObjective();
             player.calculateCommonObjectives((ObjectiveCard) common1, (ObjectiveCard) common2);
             int totalPoints = player.getPoints();
-            finalPoints.put(player, totalPoints);
+            String playerName = player.getName();
+            scoreTrack.updateScoreTrack(playerName, totalPoints);
         }
-        return finalPoints;
     }
 
 
@@ -257,7 +258,7 @@ public class Game {
 
     /**
      *
-     * @return
+     * @return next_Winner
      */
 
     public String get_next_winner_name(){
