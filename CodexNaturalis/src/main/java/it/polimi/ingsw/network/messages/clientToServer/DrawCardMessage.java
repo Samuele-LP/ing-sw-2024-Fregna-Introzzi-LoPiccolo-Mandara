5e18f7b21476acc74f4c1d5684b16b4d974c7180
federicointrozzi <federico.introzzi@mail.polimi.it>
2024-04-15 18:23:48 +0200

    Draw card message modified

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/clientToServer/DrawCardMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/clientToServer/DrawCardMessage.java
@@ -1,23 +1,24 @@
 package it.polimi.ingsw.network.messages.clientToServer;
 
 import it.polimi.ingsw.network.messages.Message;
+import it.polimi.ingsw.model.enums.PlayerDrawChoice;
 
 /**
  * Message that contains the player's draw choice
  */
 public class DrawCardMessage extends Message {
 
-    private final String choice;
+    private final PlayerDrawChoice playerChoice;
 
-    public DrawCardMessage(String choice) {
-        this.choice = choice;
+    public DrawCardMessage(PlayerDrawChoice playerChoice) {
+        this.playerChoice = playerChoice;
     }
 
     /**
      *
      * @return the choice
      */
-    public String getChoice() {
-        return choice;
+    public PlayerDrawChoice getChoice() {
+        return playerChoice;
     }
 }
