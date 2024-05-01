525ef2f23a2940442f1e6bcca1d2a4cfdff4c9d7
Samuele Lo Piccolo <samuele.lopiccolo@mail.polimi.it>
2024-05-01 12:05:20 +0200

    Modified ClientSocket to receive messages and passing them onto the ClientController in separate threads

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/client/ClientSocket.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/client/ClientSocket.java
@@ -1,13 +1,19 @@
 package it.polimi.ingsw.network.socket.client;
 
+import it.polimi.ingsw.controller.ClientSideMessageListener;
 import it.polimi.ingsw.network.commonData.ConstantValues;
 import it.polimi.ingsw.network.ClockTransmitter;
 import it.polimi.ingsw.network.messages.ClientToServerMessage;
 import it.polimi.ingsw.network.messages.Message;
+import it.polimi.ingsw.network.messages.ServerToClientMessage;
 import it.polimi.ingsw.network.socket.server.Server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class ClientSocket extends Thread{
 
@@ -15,41 +21,50 @@ public class ClientSocket extends Thread{
      * Debugging
      */
     String className = ClientSocket.class.getName();
-
+    private boolean connectionActive;
     private Socket clientSocket;
-
     private ObjectInputStream input;
     private ObjectOutputStream output;
-
+    private final LinkedList<ServerToClientMessage> messageQueue= new LinkedList<>();
+    private final ClientSideMessageListener listener;
     private final ClockTransmitter socketClock;
 
-    public ClientSocket() {
-        startConnection(ConstantValues.serverIp, ConstantValues.socketPort);
+    public ClientSocket(ClientSideMessageListener listener) {
+        this.listener = listener;
+        connectionActive=true;
+        startConnection(ConstantValues.serverIp,ConstantValues.socketPort);
         socketClock = new ClockTransmitter();
     }
-
     /**
-     * Method used to read incoming messages and do what they want
-     * Note: "run" is a commonly used name. Confusion may happen. Maybe rename it ?
+     * Method used to read incoming messages, runs indefinitely as a thread until the connection is closed.
      */
-    public void run(){
-        while(true){
+    public void receiveMessages(){
+        while(connectionActive){
             try{
-                Message message = (Message) input.readObject();
+                ServerToClientMessage message = (ServerToClientMessage) input.readObject();
+                messageQueue.add(message);
             } catch (IOException | ClassNotFoundException e){ // AGGIUNGERE ALTRE ECCEZIONI !?
                 // NB: since there are multiple catch, "e" is final (imposed by Java)
-
+                connectionActive=false;
                 System.out.println("Error with connection!");
                 System.exit(-1);
             }
         }
     }
-
     /**
-     * Starts the connection between Client and Server
-     *
-     * @param serverIP
-     * @param socketPort
+     * Runs indefinitely as a thread to pass messages onto the ClientController and handle them until the connection is closed.
+     */
+    public void passMessages(){
+        while (connectionActive) {
+            if(!messageQueue.isEmpty()){
+                ServerToClientMessage message = messageQueue.pop();
+                message.execute(listener);
+            }
+        }
+    }
+    /**
+     * Starts the connection between Client and Server. If an error occurs during connection it tries again
+     * a pre-set number of times before giving up.
      */
     public void startConnection(String serverIP, int socketPort){
         boolean connectionEstablished = false;
@@ -86,16 +101,15 @@ public class ClientSocket extends Thread{
 
     /**
      * Ends the connection between Client and Server
-     *
-     * @throws IOException
+     * @throws IOException if an error has occurred while ending the connection
      */
     public void stopConnection() throws IOException{
         input.close();
         output.close();
         clientSocket.close();
+        connectionActive=false;
         System.out.println("Connection ended successfully!");
     }
-
     /**
      * Sends a message to the server
      */
