26609051dde770284e054fa1131301270238b9d0
Samuele Lo Piccolo <samuele.lopiccolo@mail.polimi.it>
2024-05-02 18:37:13 +0200

    Modified messages to remove redundant information

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/serverToClient/EndPlayerTurnMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/serverToClient/EndPlayerTurnMessage.java
@@ -6,33 +6,9 @@ import it.polimi.ingsw.network.messages.ServerToClientMessage;
 import java.util.List;
 
 /**
- * Message used to inform a player that their turn has ended,it also contains the player's
- * updated hand data and the playing field update data after a card has been drawn.
+ * Message used to inform a player that their turn has ended.
  */
 public class EndPlayerTurnMessage extends ServerToClientMessage {
-    private final SharedFieldUpdateMessage sharedField;
-    private final List<Integer> updatedPlayerHand;
-    public EndPlayerTurnMessage(SharedFieldUpdateMessage sharedField, List<Integer> updatedPlayerHand) {
-        this.sharedField = sharedField;
-        this.updatedPlayerHand = updatedPlayerHand;
-    }
-
-    /**
-     *
-     * @return an ArrayList containing the ID's of the cards in a player's hand
-     */
-    public List<Integer> getUpdatedPlayerHand() {
-        return updatedPlayerHand;
-    }
-
-    /**
-     *
-     * @return a SharedFieldUpdateMessage that contains information about the shared field after a card has been drawn
-     */
-    public SharedFieldUpdateMessage getSharedField() {
-        return sharedField;
-    }
-
     @Override
     public void execute(ClientSideMessageListener lis) {
         lis.handle(this);
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/serverToClient/SendDrawncardMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/serverToClient/SendDrawncardMessage.java
@@ -3,24 +3,25 @@ package it.polimi.ingsw.network.messages.serverToClient;
 import it.polimi.ingsw.controller.ClientSideMessageListener;
 import it.polimi.ingsw.network.messages.ServerToClientMessage;
 
+import java.util.List;
+
 /**
  * Sends a player the card they have drawn and the changes made to the common field made by the draw.
  */
 public class SendDrawncardMessage extends ServerToClientMessage {
     private final SharedFieldUpdateMessage sharedField;
-    private final int drawnCardID;
+    private final List<Integer> updatedPlayerHand;
 
-    public SendDrawncardMessage(SharedFieldUpdateMessage sharedField, int drawnCardID) {
+    public SendDrawncardMessage(SharedFieldUpdateMessage sharedField, List<Integer> updatedPlayerHand) {
         this.sharedField = sharedField;
-        this.drawnCardID = drawnCardID;
+        this.updatedPlayerHand = updatedPlayerHand;
     }
 
     /**
-     *
      * @return the ID of the card that has been drawn
      */
-    public int getDrawnCardID() {
-        return drawnCardID;
+    public List<Integer> getDrawnCardID() {
+        return updatedPlayerHand;
     }
 
     /**
