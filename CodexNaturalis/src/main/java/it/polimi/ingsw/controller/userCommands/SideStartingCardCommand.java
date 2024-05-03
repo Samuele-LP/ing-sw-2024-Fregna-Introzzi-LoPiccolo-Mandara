d0d53f0b2e9a02a1db2aed65228bf0b6091f6842
vital <vitaliano.mandara@mail.polimi.it>
2024-05-03 10:56:07 +0200

    update SideStartingCardCommand

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/userCommands/SideStartingCardCommand.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/userCommands/SideStartingCardCommand.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.controller.userCommands;
 
-public class SideStartingCardCommand {
+public class SideStartingCardCommand extends UserCommand{
     private final String command = "str_side";
     private final int side;
 
     public SideStartingCardCommand(int side){
         this.side = side;
+    }
+
+    @Override
+    public void sendCommand(UserListener lis) {
+        lis.receiveCommand(this);
     }
 }
