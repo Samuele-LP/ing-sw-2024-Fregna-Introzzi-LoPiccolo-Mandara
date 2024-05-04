44979e8728f74c5d42c2e9aa44d160f4b452dda3
FregnaFrancesco <francesco.fregna@mail.polimi.it>
2024-05-04 09:45:43 +0200

    ClientSocketTest created successfully

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/network/socket/client/ClientSocketTest.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.network.socket.client;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientSocketTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void receiveMessages() {
+    }
+
+    @Test
+    public void passMessages() {
+    }
+
+    @Test
+    public void startConnection() {
+    }
+
+    @Test
+    public void stopConnection() {
+    }
+
+    @Test
+    public void send() {
+    }
+}
