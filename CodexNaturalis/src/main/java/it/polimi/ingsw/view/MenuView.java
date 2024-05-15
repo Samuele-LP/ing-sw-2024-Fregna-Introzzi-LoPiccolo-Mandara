af29b4923668ffca238762050db42496b1417afe
vital <vitaliano.mandara@mail.polimi.it>
2024-05-15 10:48:14 +0200

    Creation of the Package with Resource cards.png

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/MenuView.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/MenuView.java
@@ -18,7 +18,7 @@ public class MenuView {
             {"H", "Help", "ask information"},
             {"N", "Set_Name", "set name"},
             {"NP", "Num_Players", "get number of players"},
-            {"R", "Rules", "list of rules"}
+            {"Resource", "Rules", "list of rules"}
     };
 
     String[][] gameMenuOptions = {
